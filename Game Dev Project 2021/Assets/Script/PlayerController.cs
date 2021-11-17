f9bfef3bd7dd4cddff11413ca079e8a31f2c7f81
DavidIGD <DavidIGD@outlook.com>
2021-11-17 13:44:07 +0000

    11/17

--- Game Dev Project 2021/Assets/Script/PlayerController.cs
+++ Game Dev Project 2021/Assets/Script/PlayerController.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 5f;
+    private Rigidbody2D playerRigidbody;
+    private BoxCollider2D playerCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveCharacter();
-    }
-
-    void MoveCharacter()
-    {
-        if (Input.GetKey("W"))
-        {
-
-        }
+        float dirX = Input.GetAxisRaw("Horizontal");
+        playerRigidbody.velocity = new Vector2(dirX * 5f, playerRigidbody.velocity.y);
+        float dirY = Input.GetAxisRaw("Vertical");
+        playerRigidbody.velocity = new Vector2(dirY * 5f, playerRigidbody.velocity.y);
     }
 }
