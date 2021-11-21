93521a750eccdd2f39a411475bf65ac52aa4511f
DavidIGD <DavidIGD@outlook.com>
2021-11-21 23:35:24 +0000

    Movement Script

--- Game Dev Project 2021/Assets/Script/PlayerController.cs
+++ Game Dev Project 2021/Assets/Script/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D playerRigidbody;
-    private BoxCollider2D playerCollider;
+    public float speed = 6f;
+    Vector2 movement;
+    Vector2 mousePos;
+    public Rigidbody2D playerRigidbody;
+    public BoxCollider2D playerCollider;
+    public Camera cam1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
+        mousePos = cam1.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidbody.MovePosition(playerRigidbody.position + movement * speed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - playerRigidbody.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        playerRigidbody.rotation = angle;
     }
 }
