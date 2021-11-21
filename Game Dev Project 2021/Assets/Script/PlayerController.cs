c34f94a78a5c4f805ce9602ed3d17134a5f50ae6
DavidIGD <DavidIGD@outlook.com>
2021-11-21 20:56:42 +0000

    ?

--- Game Dev Project 2021/Assets/Script/PlayerController.cs
+++ Game Dev Project 2021/Assets/Script/PlayerController.cs
@@ -10,16 +10,12 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody2D>();
-        playerCollider = GetComponent<BoxCollider2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        playerRigidbody.velocity = new Vector2(dirX * 5f, playerRigidbody.velocity.y);
-        float dirY = Input.GetAxisRaw("Vertical");
-        playerRigidbody.velocity = new Vector2(dirY * 5f, playerRigidbody.velocity.y);
+
     }
 }
