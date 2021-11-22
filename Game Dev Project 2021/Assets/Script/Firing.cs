2301775bdc00fb91a0dc88df86f84289a9dbca99
DavidIGD <DavidIGD@outlook.com>
2021-11-22 00:31:57 +0000

    Firing Script and bullets

---
+++ Game Dev Project 2021/Assets/Script/Firing.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
