07778be2f539fd34156300acac8a841c89e6cb94
DavidIGD <DavidIGD@outlook.com>
2021-11-23 09:45:24 +0000

    HealthImplemented

---
+++ Game Dev Project 2021/Assets/Script/Alien2Heatlh.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien2Heatlh : MonoBehaviour
+{
+    public int Health = 9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Bullet(Clone)")
+        {
+            Health -= 1;
+            if (Health < 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Game Dev Project 2021/Assets/Script/PlayerController.cs
+++ Game Dev Project 2021/Assets/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D playerRigidbody;
     public BoxCollider2D playerCollider;
     public Camera cam1;
-
+    public int Health = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,5 +34,17 @@ public class PlayerController : MonoBehaviour
         Vector2 lookDir = mousePos - playerRigidbody.position; //uses both the player and mouse position to 
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f; //calculates the angle between the mouse and player using the Mathf.Atan2 function which uses vectors to find the angle from the origin also converts the answer from radians to degrees and takes away 90 degrees otherwise the charatcer would be facing the wrong direction
         playerRigidbody.rotation = angle; //rotates the player to face the mouse
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Alien")
+        {
+            Health -= 1;
+            if(Health < 1)
+            {
+                Destroy(gameObject);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
     }
 }
